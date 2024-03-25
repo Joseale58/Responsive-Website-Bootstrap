5083cfb372bf7d485fca0850db7c46933fb20e27
Joseale58 <70922734+Joseale58@users.noreply.github.com>
2024-03-24 23:07:00 -0500

    Editar post + cambio side menu

---
+++ admin/script_sidemenu.js
@@ -0,0 +1,6 @@
+//SideMenu
+const hamBurger = document.querySelector(".toggle-btn");
+
+hamBurger.addEventListener("click", function () {
+  document.querySelector("#sidebar").classList.toggle("expand");
+});
